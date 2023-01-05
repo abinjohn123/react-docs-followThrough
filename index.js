56f8cbbcc3c9c283970b225390b6d55e8c88b476
Abin John <abinjohnanto@gmail.com>
2023-01-05 08:15:12 +0530

    passing props

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Importing & Exporting Components
+// Passing Props
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Avatar({ person, id }) {
+  return <img src={'https://i.pravatar.cc/150?img=' + id} alt={person} />;
+}
+
+function App() {
+  return (
+    <div>
+      <Avatar person="AJ" id="0" />
+      <Avatar person="SG" id="2" />
+      <Avatar person="GU" id="4" />
+    </div>
+  );
+}
+
+root.render(<App />);
