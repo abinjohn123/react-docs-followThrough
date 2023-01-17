738512cc525ffaa5525bd27fcb281dab9e960261
Abin John <abinjohnanto@gmail.com>
2023-01-17 19:30:04 +0530

    add items within array

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const { useState } = React;
 
-function App() {
-  const [counters, setcounter] = useState([23, 55, 67]);
-  function handleIncrementClick(index) {
-    setcounter(
-      counters.map((counter, i) => {
-        if (i === index) return counter + 1;
+const initialArtists = [
+  { id: 0, name: 'Marta Colvin Andrade' },
+  { id: 1, name: 'Lamidi Olonade Fakeye' },
+  { id: 2, name: 'Louise Nevelson' },
+];
 
-        return counter;
-      })
-    );
+let nextId = 3;
+
+function App() {
+  const [input, setInput] = useState('');
+  const [list, setList] = useState([...initialArtists]);
+
+  function addItemHandler() {
+    console.log(list);
+    const index = 2;
+    setList([
+      ...list.slice(0, index),
+      { id: nextId++, name: input },
+      ...list.slice(index),
+    ]);
   }
+
   return (
     <div>
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        type="text"
+      />
+      <button onClick={addItemHandler}>Add</button>
       <ul>
-        {counters.map((counter, i) => (
-          <li key={i}>
-            {counter}{' '}
-            <button
-              onClick={() => {
-                handleIncrementClick(i);
-              }}
-            >
-              +1
-            </button>
-          </li>
+        {list.map((entry, i) => (
+          <li key={i}>{entry.name}</li>
         ))}
       </ul>
     </div>
